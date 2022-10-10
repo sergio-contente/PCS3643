48ad6b9f5981cb23cb145d9f7e76725e9076dc7b
sergio-contente <sergio.mcontente@gmail.com>
2022-10-10 13:59:31 -0300

    ✅  Voo tests

--- MyProj/FlightApp/tests.py
+++ MyProj/FlightApp/tests.py
@@ -87,5 +87,74 @@ class HorarioRealTest(TestCase):
 	def test_delete(self):
 		HorarioReal.objects.filter(id_real=1).delete()
 		self.assertEqual(HorarioReal.objects.count(), 0)
+	
+class RotaTest(TestCase):
+	"""
+	Testa o CRUD de Rota
+	"""
+	@classmethod
+	#CRUD TEST
+	def setUpTestData(cls):
+		Rota.objects.create(id_rota = 1,
+			aeroporto_destino = 'GRU',
+			aeroporto_saida = 'JFK',
+		)
+
+	def test_read(self):
+		rota_1 = Rota.objects.get(id_rota = 1)
+		self.assertEqual(rota_1.id_rota, 1)
+		self.assertEqual(rota_1.aeroporto_destino, 'GRU')
+		self.assertEqual(rota_1.aeroporto_saida, 'JFK')
+
+	def test_update(self):
+		Rota.objects.filter(id_rota = 1).update(aeroporto_destino = 'MCO')
+		rota_2 = Rota.objects.get(id_rota = 1)
+		self.assertEqual(rota_2.id_rota, 1)
+		self.assertEqual(rota_2.aeroporto_destino, 'MCO')
+		self.assertEqual(rota_2.aeroporto_saida, 'JFK')
+
+	def test_delete(self):
+		Rota.objects.filter(id_rota = 1).delete()
+		self.assertEqual(Rota.objects.count(), 0)
+
+class TestVoo(TestCase):
+	"""
+	Tests for the Voo model.
+	"""
+	@classmethod
+	#CRUD TEST
+	def setUpTestData(cls):
+		estado_voo = Status.objects.create(id_status = 10, status_voo = 'Programado')
+		rota_voo = Rota.objects.create(id_rota = 13, aeroporto_destino = 'JFK', aeroporto_saida = 'GRU')
+		previsao_voo = HorarioPrevisto.objects.create(id_previsao = 13,
+			partida_prevista = datetime.datetime(2022, 10, 10, 22, 30, 10, tzinfo=datetime.timezone.utc),
+			chegada_prevista = datetime.datetime(2022, 10, 11, 22, 30, 10, tzinfo=datetime.timezone.utc)
+			)
+		real_voo = HorarioReal.objects.create(id_real = 13,
+			partida_real = datetime.datetime(2022, 10, 10, 22, 40, 20, tzinfo=datetime.timezone.utc), 
+			chegada_real = datetime.datetime(2022, 10, 11, 22, 35, 10, tzinfo=datetime.timezone.utc)
+		)
+		Voo.objects.create(codigo_voo = 'TAM0001',
+		companhia_aerea = "TAM",
+		estado = estado_voo,
+		rota = rota_voo,
+		previsao = previsao_voo,
+		real = real_voo)
+
+	def test_read(self):
+		voo_1 = Voo.objects.get(codigo_voo = 'TAM0001')
+		self.assertEqual(voo_1.codigo_voo, 'TAM0001')
+		self.assertEqual(voo_1.companhia_aerea, 'TAM')
+	
+	#TO-DO: Update
+	# def test_update(self):
+	# 	voo_1 = Voo.objects.filter(codigo_voo = 'TAM0001').update(companhia_aerea = 'GOL')
+	# 	self.assertEqual(voo_1.companhia_aerea, "GOL")
+	
+	def test_delete(self):
+		Voo.objects.filter(codigo_voo = 'TAM0001').delete()
+		self.assertEqual(Voo.objects.count(), 0)
+
+
 
 
