5f38ace36b801719538f7c18caacee1db9529820
sergio-contente <sergio.mcontente@gmail.com>
2022-10-10 11:18:43 -0300

    ✨  Add views of flight app

--- MyProj/FlightApp/views.py
+++ MyProj/FlightApp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def main_flight(request):
+	return render(request, "main_flight.html")
--- MyProj/MyProj/urls.py
+++ MyProj/MyProj/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from book import views
+from FlightApp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('FIRST/', views.bookview),
+    path('main_flight/', views.main_flight),
 ]
