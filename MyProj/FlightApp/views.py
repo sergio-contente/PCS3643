3ffe692f4da1abbb27762ab636bbc6c5315d5f70
sergio-contente <sergio.mcontente@gmail.com>
2022-11-10 21:59:44 -0300

    🎨 New login logic initial commit

--- MyProj/FlightApp/views.py
+++ MyProj/FlightApp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect, HttpResponseNotAllowed
 from django.shortcuts import render
 import datetime
+from django.contrib import messages
 
 from FlightApp.forms import LoginForm
 
@@ -31,22 +32,28 @@ def Login(request):
     admin_cred = {'username': 'admin', 'password': 'admin'}
     operador = {'username': 'operador', 'password': 'operador'}
     funcionario = {'username': 'funcionario', 'password': 'funcionario'}
-    print(">>")
+    global tentativas
+    tentativas = 0
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = LoginForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
+        if (tentativas == 2):
+              messages.error(request, "Número de tentativas ultrapassado. Acesso bloqueado.")
+              return HttpResponseRedirect('/')
+            # check whether it's valid:
+        elif form.is_valid():
             # process the data in form.cleaned_data as required
             credentials = form.cleaned_data
             if credentials == admin_cred or credentials == operador or credentials == funcionario:
+                tentativas = 0
                 return HttpResponseRedirect('/ListaVoos/')
             else:
+                tentativas = tentativas + 1
+                messages.warning(request, f"Login inválido. Número de tentativas restantes: {tentativas%3}")
                 return HttpResponseRedirect('/')
             # ...
             # redirect to a new URL:
-        return render(request, "ListaVoos.html")
 
     # if a GET (or any other method) we'll create a blank form
     else:
