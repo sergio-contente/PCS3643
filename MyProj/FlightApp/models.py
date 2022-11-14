3598a36e08a65f6add0d5a7ab1478912e23de121
Luis del Llano <luisdellano@usp.br>
2022-11-14 18:06:53 -0300

    🩹 fix: models fields

--- MyProj/FlightApp/models.py
+++ MyProj/FlightApp/models.py
@@ -1,52 +1,63 @@
 from django.db import models
-ESTADOS_VOO = [
-	"ESTACIONADO",
-	"ATERRISSANDO",
-]
-
+import uuid
+from FlightApp.utils import flightStatus
 # Create your models here.
+
+
 class HorarioReal(models.Model):
-	id_real = models.IntegerField(primary_key=True)
-	partida_real = models.DateTimeField(null=True)
-	chegada_real = models.DateTimeField(null=True)
-	class Meta:
-		db_table = 'horarios_reais'
+    id_real = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    partida_real = models.DateTimeField(null=True, default=None)
+    chegada_real = models.DateTimeField(null=True, default=None)
+
+    class Meta:
+        db_table = 'horarios_reais'
+
 
 class Rota(models.Model):
-	id_rota = models.IntegerField(primary_key=True)
-	aeroporto_destino = models.CharField(max_length=3, null=False)
-	aeroporto_saida = models.CharField(max_length=3, null=False)
-	class Meta:
-			db_table = 'rotas'
+    id_rota = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    aeroporto_destino = models.CharField(max_length=3, null=False)
+    aeroporto_saida = models.CharField(max_length=3, null=False)
+
+    class Meta:
+        db_table = 'rotas'
+
 
 class Status(models.Model):
-	id_status = models.IntegerField(primary_key=True)
-	status_voo = models.CharField(max_length=20, null=False)
-	class Meta:
-  		db_table ='status'
+    id_status = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    status_voo = models.CharField(
+        max_length=20, null=False, default=flightStatus[0])
+
+    class Meta:
+        db_table = 'status'
+
 
 class HorarioPrevisto(models.Model):
-	id_previsao = models.IntegerField(primary_key=True)
-	partida_prevista = models.DateTimeField(null=False)
-	chegada_prevista = models.DateTimeField(null=False)
-	class Meta:
-			db_table ='horarios_previstos'
+    id_previsao = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    partida_prevista = models.DateTimeField(null=False)
+    chegada_prevista = models.DateTimeField(null=False)
+
+    class Meta:
+        db_table = 'horarios_previstos'
+
 
 class Voo(models.Model):
-	codigo_voo = models.CharField(primary_key=True, max_length=20)
-	companhia_aerea = models.CharField(max_length=10, null=False)
-	estado = models.ForeignKey(Status, on_delete=models.CASCADE)
-	rota = models.ForeignKey(Rota, on_delete=models.CASCADE)
-	previsao = models.ForeignKey(HorarioPrevisto, on_delete=models.CASCADE)
-	real = models.ForeignKey(HorarioReal, on_delete=models.CASCADE)
-	class Meta:
-		db_table = 'voos'
+    codigo_voo = models.CharField(primary_key=True, max_length=20)
+    companhia_aerea = models.CharField(max_length=10, null=False)
+    estado = models.ForeignKey(Status, on_delete=models.CASCADE)
+    rota = models.ForeignKey(Rota, on_delete=models.CASCADE)
+    previsao = models.ForeignKey(HorarioPrevisto, on_delete=models.CASCADE)
+    real = models.ForeignKey(HorarioReal, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'voos'
+
 
 class Relatorio(models.Model):
-	id_relatorio = models.IntegerField(primary_key=True)
-	codigo_voo = models.ForeignKey(Voo, on_delete=models.CASCADE)
-	tipo = models.CharField(max_length=20, null=False)
-	tempo_real_atraso = models.DateTimeField(null=False)
-	data_geracao = models.DateTimeField(null=False)
-	class Meta:
-		db_table ='relatorios'
+    id_relatorio = models.UUIDField(primary_key=True)
+    codigo_voo = models.ForeignKey(Voo, on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=20, null=False)
+    tempo_real_atraso = models.DateTimeField(null=False)
+    data_geracao = models.DateTimeField(null=False)
+
+    class Meta:
+        db_table = 'relatorios'
